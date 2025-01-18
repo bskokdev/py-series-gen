85c03fcaba22adc4d36cedf34ba1e6c00a3ed873
Boris Skok <skokcmd@gmail.com>
2025-01-18 23:35:18 +0100

    added more help strings to the argument parser

--- generator/cli/arguments.py
+++ generator/cli/arguments.py
@@ -16,18 +16,30 @@ specific_target_args: Dict[TargetType, List[Tuple[str, str, Any]]] = defaultdict
     list,
     {
         TargetType.kafka: [
-            ("--bootstrap-server", "bootstrap_server", str),
-            ("--topic", "kafka_topic", str),
+            ("--bootstrap-server", "bootstrap_server", str, "Address of the broker"),
+            ("--port", "port", str, "Port at which the broker process is running"),
+            ("--topic", "kafka_topic", str, "Kafka topic to send the data to"),
         ]
     },
 )
 
 
 def _attach_default_args(parser: ArgumentParser):
-    parser.add_argument("--target", dest="target", type=str)
-    parser.add_argument("--batch-size", dest="batch_size", type=int)
     parser.add_argument(
-        "--stream", dest="is_stream", action="store_true", default=False
+        "--target", dest="target", type=str, help="Destination to publish the data to"
+    )
+    parser.add_argument(
+        "--batch-size",
+        dest="batch_size",
+        type=int,
+        help="Single batch (iteration) size",
+    )
+    parser.add_argument(
+        "--stream",
+        dest="is_stream",
+        action="store_true",
+        default=False,
+        help="Should the batch be sent repeatedly",
     )
 
 
@@ -38,8 +50,8 @@ def create_parser_with_all_args() -> ArgumentParser:
     # Add all target-specific arguments with their target type as a prefix
     for target_type, args in specific_target_args.items():
         group = parser.add_argument_group(f"{target_type.name} specific arguments")
-        for argument, variable, dtype in args:
-            group.add_argument(argument, dest=variable, type=dtype)
+        for argument, variable, dtype, help_str in args:
+            group.add_argument(argument, dest=variable, type=dtype, help=help_str)
 
     return parser
 
