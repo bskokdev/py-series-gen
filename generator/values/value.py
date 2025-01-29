10aac71610fba1c670fd86c633c3c5bf7b4a94a0
Boris Skok <skokcmd@gmail.com>
2025-01-29 13:14:38 +0100

    changed value data type from str to Any

--- generator/values/value.py
+++ generator/values/value.py
@@ -1,14 +1,14 @@
 import json
 from abc import ABC
 from dataclasses import dataclass
-from typing import List
+from typing import Any, List
 
 
 @dataclass
 class Value(ABC):
     """Generic data class which represents an object holding some data."""
 
-    data: List[str]
+    data: List[Any]
 
     def serialize(self):
         return json.dumps(self.data)
