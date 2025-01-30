eac865461c63936bc38ff70c67751f15d0f5a766
Boris Skok <skokcmd@gmail.com>
2025-01-30 14:21:13 +0100

    implemented better request sending, and unit tests for http publisher

--- generator/publishers/http_publisher.py
+++ generator/publishers/http_publisher.py
@@ -1,5 +1,5 @@
 import logging
-from time import sleep
+import time
 from typing import Generator
 
 import requests
@@ -26,12 +26,10 @@ class HttpPublisher(Publisher):
         target: HttpTarget,
         max_retries: int = 5,
         backoff_factor: int = 1,
-        status_forcelist=[429, 500, 502, 503, 504],
     ):
         super().__init__(generator_fun, target)
         self._max_retries = max_retries
         self._backoff_factor = backoff_factor
-        self._status_forcelist = status_forcelist
 
     def _compute_backoff_delay(self, response: Response, attempt: int) -> float:
         """Computes the delay the publisher should wait before attempting
@@ -65,7 +63,7 @@ class HttpPublisher(Publisher):
             value (Value): Value DTO with data to be published
 
         Raises:
-            ConnectionError: Raised if we reach the max number of retries with an error response
+            HttpError: Raised if we reach the max number of retries with an error response
 
         Returns:
             Response | None: Response object returned by the server
@@ -76,19 +74,30 @@ class HttpPublisher(Publisher):
         for attempt in range(self._max_retries):
             try:
                 res = requests.post(self._target.endpoint_url, json=value.data)
-                if res.status_code == 200:
+                if res.ok:
                     return res
-                elif res.status_code in self._status_forcelist:
-                    sleep(self._compute_backoff_delay(response=res, attempt=attempt))
                 else:
-                    sleep(self._compute_backoff_delay(response=res, attempt=attempt))
-                    res.raise_for_status()
-            except HTTPError as e:
-                if attempt == self._max_retries - 1:
-                    logger.warning("Reached max number of retries with an error status")
-                    raise ConnectionError(
-                        f"Failed to publish data via HTTP to {self._target.endpoint_url}: {e}"
+                    # For each error response code, we'll try to re-send the data,
+                    # however, the timeout will be incrementally increased.
+                    # In case of Retry-After header, we use it instead of the incremented timeout.
+                    backoff_delay = self._compute_backoff_delay(
+                        response=res, attempt=attempt
                     )
+                    time.sleep(backoff_delay)
+
+                    logger.warning(
+                        f"Received an error response code, trying again after: {backoff_delay} seconds"
+                    )
+                    res.raise_for_status()
+            except HTTPError:
+                if attempt == self._max_retries - 1:
+                    raise HTTPError(
+                        f"Failed to publish data via HTTP POST to {self._target.endpoint_url} with max number of retries"
+                    )
+            except ConnectionError:
+                raise ConnectionError(
+                    f"Failed to connect to {self._target.endpoint_url}, check the URL"
+                )
 
         return None
 
@@ -100,7 +109,17 @@ class HttpPublisher(Publisher):
             return
 
         for generated_value in self._generator(self._target.batch_size):
-            response = self._send_value_to_endpoint(value=generated_value)
-            logger.info(
-                f"Sent data to {self._target.endpoint_url} with response code: {response.status_code}"
-            )
+            try:
+                response = self._send_value_to_endpoint(value=generated_value)
+                logger.info(
+                    f"Sent data to {self._target.endpoint_url} with response code: {response.status_code}"
+                )
+            except HTTPError as ex:
+                # We failed to send the data with max number of retries,
+                # so we don't send any more data to not spam the server
+                logger.error(f"{ex}... Not sending more data")
+                break
+            except ConnectionError as ex:
+                # Publisher has failed to connect to the endpoint_url
+                logger.error(f"{ex}... Not sending more data")
+                break
--- generator/tests/unit/test_http.py
+++ generator/tests/unit/test_http.py
@@ -1,59 +1,46 @@
+from unittest.mock import patch
+
 import pytest
 from publishers import HttpPublisher
 from publishers.targets import HttpTarget
-from requests import Response
+from requests import HTTPError, Response
 from values import Value
 
 
 @pytest.fixture
-def valid_response() -> Response:
-    response = Response()
-    response.headers = {}
-    response.status_code = 200
-    return response
-
-
-@pytest.fixture
-def response_with_retry_after() -> Response:
-    response = Response()
-    response.status_code = 429
-    response.headers = {"Retry-After": "5"}
-    return response
-
-
-@pytest.fixture
-def http_target_fixture() -> HttpTarget:
+def http_target() -> HttpTarget:
     return HttpTarget(endpoint_url="https://example.com/")
 
 
 @pytest.fixture
-def http_publisher_fixture(http_target_fixture: HttpTarget) -> HttpPublisher:
+def http_publisher(http_target: HttpTarget) -> HttpPublisher:
     return HttpPublisher(
         generator_fun=lambda batch_s: [Value(data=[i]) for i in range(batch_s)],
-        target=http_target_fixture,
-        max_retries=16,
-        backoff_factor=2,
+        target=http_target,
+        max_retries=4,
+        backoff_factor=1,
     )
 
 
 @pytest.mark.unit_test
-def test_compute_backoff_delay_with_retry_after(
-    response_with_retry_after: Response, http_publisher_fixture: HttpPublisher
-):
+def test_compute_backoff_delay_with_retry_after(http_publisher: HttpPublisher):
+    response_with_retry_after = Response()
+    response_with_retry_after.status_code = 429
+    response_with_retry_after.headers = {"Retry-After": "5"}
     # retry-after header has a priority over the backoff strategy
-    negative_attempt_delay = http_publisher_fixture._compute_backoff_delay(
+    negative_attempt_delay = http_publisher._compute_backoff_delay(
         response=response_with_retry_after, attempt=-1
     )
 
-    first_delay = http_publisher_fixture._compute_backoff_delay(
+    first_delay = http_publisher._compute_backoff_delay(
         response=response_with_retry_after, attempt=1
     )
 
-    second_delay = http_publisher_fixture._compute_backoff_delay(
+    second_delay = http_publisher._compute_backoff_delay(
         response=response_with_retry_after, attempt=2
     )
 
-    twelfth_delay = http_publisher_fixture._compute_backoff_delay(
+    twelfth_delay = http_publisher._compute_backoff_delay(
         response=response_with_retry_after, attempt=12
     )
 
@@ -64,29 +51,59 @@ def test_compute_backoff_delay_with_retry_after(
 
 
 @pytest.mark.unit_test
-def test_compute_backoff_delay_without_retry_after(
-    valid_response: Response,
-    http_publisher_fixture: HttpPublisher,
-):
-    negative_attempt_delay = http_publisher_fixture._compute_backoff_delay(
+def test_compute_backoff_delay_without_retry_after(http_publisher: HttpPublisher):
+    valid_response = Response()
+    valid_response.headers = {}
+    valid_response.status_code = 200
+    negative_attempt_delay = http_publisher._compute_backoff_delay(
         response=valid_response, attempt=-1
     )
     # backoff_factor * (2 ** (attempt - 1))
-    # 2 * (2 ** (1 - 1)) = 2.0
-    first_delay = http_publisher_fixture._compute_backoff_delay(
+    # 1 * (2 ** (1 - 1)) = 1.0
+    first_delay = http_publisher._compute_backoff_delay(
         response=valid_response, attempt=1
     )
 
-    # 2 * (2 ** (2 - 1)) = 4.0
-    second_delay = http_publisher_fixture._compute_backoff_delay(
+    # 1 * (2 ** (2 - 1)) = 2.0
+    second_delay = http_publisher._compute_backoff_delay(
         response=valid_response, attempt=2
     )
-    # 2 * (2 ** (12 - 1)) = 4096.0
-    twelfth_delay = http_publisher_fixture._compute_backoff_delay(
+    # 1 * (2 ** (12 - 1)) = 2048.0
+    twelfth_delay = http_publisher._compute_backoff_delay(
         response=valid_response, attempt=12
     )
 
     assert negative_attempt_delay == 0.0
-    assert first_delay == 2.0
-    assert second_delay == 4.0
-    assert twelfth_delay == 4096.0
+    assert first_delay == 1.0
+    assert second_delay == 2.0
+    assert twelfth_delay == 2048.0
+
+
+@pytest.mark.unit_test
+def test_send_value_to_endpoint_success(http_publisher: HttpPublisher):
+    mock_response = Response()
+    mock_response.status_code = 200
+    with patch("requests.post", return_value=mock_response):
+        result = http_publisher._send_value_to_endpoint(value=Value([1, 2, 3]))
+        assert result == mock_response
+
+
+@pytest.mark.unit_test
+def test_send_value_to_endpoint_retry(http_publisher: HttpPublisher):
+    mock_response = Response()
+    mock_response.status_code = 429
+    # intercept the post, and sleep functions
+    # set the return value of the post function
+    # verity the sleep was called `_max_retries` times
+    with patch("requests.post", return_value=mock_response), patch(
+        "time.sleep"
+    ) as mock_sleep:
+        with pytest.raises(HTTPError) as exc_info:
+            result = http_publisher._send_value_to_endpoint(value=Value([1, 2, 3]))
+            assert result is None
+            assert mock_sleep.call_count == http_publisher._max_retries
+
+        assert (
+            f"Failed to publish data via HTTP POST to {http_publisher._target.endpoint_url}"
+            in str(exc_info.value)
+        )
